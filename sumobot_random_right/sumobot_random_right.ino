4401cf17022dfddcc1344b415c14c89c04b4abbb
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2024-01-12 22:11:20 -0500

    Made changes in existing patterns

---
+++ sumobot_random_right/sumobot_random_right.ino
@@ -0,0 +1,45 @@
+// Sensor
+#define irs 8
+
+// Left Motor
+#define LF 9 // int4
+#define LB 10 // int3
+
+// Right Motor
+#define RF 11 // int2
+#define RB 12 // int1
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LF, OUTPUT);
+  pinMode(LB, OUTPUT);
+
+  pinMode(RF, OUTPUT);
+  pinMode(RB, OUTPUT);
+
+  pinMode(irs, INPUT);
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ if (digitalRead(irs) == 1) {
+  digitalWrite(LF,HIGH);
+  digitalWrite(RF,HIGH);
+  digitalWrite(LB,LOW);
+  digitalWrite(RB,LOW);
+ } else {
+  digitalWrite(LB,HIGH);
+  digitalWrite(RB,HIGH);
+  digitalWrite(LF,LOW);
+  digitalWrite(RF,LOW);
+  delay(1000);
+  digitalWrite(LB,LOW);
+  digitalWrite(RF,LOW);
+  digitalWrite(LF,HIGH);
+  digitalWrite(RB,HIGH);
+  delay(1000);
+ }
+}
